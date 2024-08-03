780855b4bb64d8871a73f0289b7351a817c1c9de
sunflowerbyte <sanjuantricia@ymail.com>
2024-08-03 15:36:04 +1200

    exercises

---
+++ Module 1/LabExercises/exercise4/exercise4.js
@@ -0,0 +1,23 @@
+function add(a, b) 
+{
+    return a + b
+}
+
+function subtract(a, b) 
+{
+    return a - b
+}
+
+function multiply(a, b) 
+{
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add(1,3))
+console.log(subtract(10,1))
+console.log(multiply(7,100))
+console.log(divide(10,2))
